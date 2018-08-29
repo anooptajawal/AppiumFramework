f4b48ac59c0ea2c2622a1eb8997d543c2bac59bc
AnoopAmbunhi <AnoopAmbunhi@github.com>
2018-08-29 11:00:54 +0400

    code for starting appium server

---
+++ src/main/java/Mobile/StartAppiumServer.java
@@ -0,0 +1,49 @@
+package Mobile;
+
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+
+public class StartAppiumServer {
+	public void startServer() {
+
+		// Setup Appium Server Variables
+		CommandLine cmd = new CommandLine("C:\\Program Files (x86)\\Appium\\node.exe");
+		cmd.addArgument("C:\\Program Files (x86)\\Appium\\node_modules\\appium\\bin\\Appium.js");
+		cmd.addArgument("--address");
+		cmd.addArgument("127.0.0.1");
+		cmd.addArgument("--port");
+		cmd.addArgument("4723");
+
+		// Launch the Appium Server
+		DefaultExecuteResultHandler handler = new DefaultExecuteResultHandler();
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(1);
+		try {
+			executor.execute(cmd, handler);
+			Thread.sleep(10000);
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void stopServer() {
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			runtime.exec("taskkill /F /IM node.exe");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		AppiumDriverLocalService appiumServer = AppiumDriverLocalService.buildDefaultService();
+		appiumServer.start();
+		appiumServer.stop();
+	}
+
+}
