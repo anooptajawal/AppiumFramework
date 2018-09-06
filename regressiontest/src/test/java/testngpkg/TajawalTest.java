c8a8437158a4c43aa9524e381a91805448394deb
AnoopAmbunhi <AnoopAmbunhi@github.com>
2018-09-06 16:57:57 +0400

    move implcit wait

--- regressiontest/src/test/java/testngpkg/TajawalTest.java
+++ regressiontest/src/test/java/testngpkg/TajawalTest.java
@@ -23,12 +23,12 @@ public class TajawalTest extends DesiredCapabilityAndroid {
     private void setUp() throws MalformedURLException {
         driver = capabilities();
         wait = new WebDriverWait(driver, 20);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
     private void selectPOS() {
         String buttonText = "Continue";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         String text = driver.findElementById("action_sticky_textview").getText();
 
         // Assert actual text on button is equal to expected
